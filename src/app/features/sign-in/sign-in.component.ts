7c0172332011210ee4d62915ecd36d75cffbb932
AvigailShlomov <anm798@gmail.com>
2025-02-09 21:23:37 +0200

    add sign in comp

---
+++ src/app/features/sign-in/sign-in.component.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [ReactiveFormsModule],
+  templateUrl: './sign-in.component.html',
+  styleUrl: './sign-in.component.scss',
+})
+export class SignInComponent {
+  private readonly formBuilder = inject(FormBuilder);
+  errorMassage = '';
+
+  sighInForm: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.sighInForm.valid) {
+      //form valid
+    } else {
+      this.errorMassage = 'Invalid Email or Password';
+      console.log('form is inValid');
+    }
+  }
+}
