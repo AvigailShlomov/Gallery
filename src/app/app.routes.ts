e4350f4ee5e3fa444dba572f72272c7701e77543
AvigailShlomov <anm798@gmail.com>
2025-02-09 19:41:28 +0200

    add sign in route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './features/sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SignInComponent },
+  { path: 'sign-in', component: SignInComponent },
+];
