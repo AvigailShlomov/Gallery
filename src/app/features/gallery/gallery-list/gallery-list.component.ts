99b80fb67e31c8e7c66f07ee1fc6e58d6b24edb8
AvigailShlomov <anm798@gmail.com>
2025-02-10 13:09:07 +0200

    orgenize+rename gallery to gallert-list

---
+++ src/app/features/gallery/gallery-list/gallery-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-list',
+  imports: [],
+  templateUrl: './gallery-list.component.html',
+  styleUrl: './gallery-list.component.scss'
+})
+export class GalleryListComponent {
+
+}
