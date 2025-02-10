dfa7574c02bc6529520be868dab53616cdb07890
AvigailShlomov <anm798@gmail.com>
2025-02-10 13:08:30 +0200

    orgenize files

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { SignInComponent } from './features/sign-in/sign-in.component';
-
-export const routes: Routes = [
-  { path: '', component: SignInComponent },
-  { path: 'sign-in', component: SignInComponent },
-];
